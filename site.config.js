c5299d3409c92fcaca5b9b1698f39bb22178442c
civileng9 <92366201+civileng9@users.noreply.github.com>
2022-01-15 11:28:38 +0530

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'd3d217a27c68f64427a495fb954aa8abbd',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
+  name: 'Civil Engineering',
   domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'Shailesh',
 
   // open graph metadata (optional)
   description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
+  socialImageTitle: 'Welcome',
   socialImageSubtitle: 'Hello World! 👋',
 
-  // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  // social username
+  twitter: 'ig_shailesh',
+  github: 'civileng9',
+  linkedin: 'in/shaileshofficial2019',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
